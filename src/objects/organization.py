3d5064e2bdec72498edc701774b07e7a65b1b35b
Caleb Druckemiller <cdruckemiller@harrisburgu.edu>
2023-02-09 13:53:12 -0500

    checkpoint

--- src/objects/organization.py
+++ src/objects/organization.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Organization:
-         id: int
-         name: str
-         address: str
-         location: str
-         notes: str
-         website: str
-         parent_organization: str
+    id: int
+    name: str
+    address: str
+    location: str
+    notes: str
+    website: str
+    parent_organization: str
