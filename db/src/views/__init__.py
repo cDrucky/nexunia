55e053b6e898119d95c607a4a759694486671bee
Caleb Druckemiller <cldruckemiller@gmail.com>
2023-03-08 20:57:50 -0500

    Endpoint Working

--- db/src/views/__init__.py
+++ db/src/views/__init__.py
@@ -1,5 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 my_blueprint = Blueprint('my_blueprint', __name__)
 
 
+@my_blueprint.route('/query')
+def query():
+    location = request.args.get('location')
+    lifecycles = request.args.getlist('lifecycles')
+    print(f"Locations: {location} Lifecylces: {lifecycles}")
+    return "Success"
