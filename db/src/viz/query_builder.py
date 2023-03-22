cdcce62f5eb24a2f37110863b78024f96e87f9b2
Caleb Druckemiller <cldruckemiller@gmail.com>
2023-03-22 19:37:03 -0400

    update default query + add services

--- db/src/viz/query_builder.py
+++ db/src/viz/query_builder.py
@@ -1,18 +1,23 @@
 default_query = """
 MATCH (o:Organization)
-WHERE o.location = 'York'
-MATCH (o)-[:SERVICES]->(l:Location)
-MATCH (o)-[:PROVIDES]->(lc:Lifecycle)
-RETURN o, l, lc
+MATCH (o)-[:SERVICES]->(l:Location) WHERE l.name = 'Harrisburg'
+MATCH (o)-[:PROVIDES]->(lc:Lifecycle) WHERE lc.name = 'Growth'
+MATCH (o)-[:HANDLES]->(s:Service)
+WHERE s.name IN ['Funding'] 
+RETURN o, l, lc, s
 """
 
 
-def query_builder(location, lifecycle):
+def query_builder(location="Harrisburg", lifecycle="Growth", services="Funding", **kwargs):
     _query = "MATCH (o:Organization) "
     _query += "MATCH (o)-[:SERVICES]->(l:Location) "
-    _query += f"WHERE l.name = '{location}' "
+    _query += f"WHERE l.name in {location} "
     _query += "MATCH (o)-[:PROVIDES]->(lc:Lifecycle) "
-    _query += f"WHERE lc.name = '{lifecycle}' "
-    _query += "RETURN o, l, lc"
-
+    _query += f"WHERE lc.name in {lifecycle} "
+    _query += "MATCH (o)-[:HANDLES]->(s:Service) "
+    _query += f"WHERE s.name IN {services} "
+    _query += "RETURN o, l, lc, s"
     return _query
+
+
+
