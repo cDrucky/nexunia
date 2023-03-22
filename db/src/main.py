322c86a632e2ba17321b76ffd1c2bd15eee0b7aa
Caleb Druckemiller <cldruckemiller@gmail.com>
2023-03-22 19:37:11 -0400

    Make graph concentric

--- db/src/main.py
+++ db/src/main.py
@@ -7,8 +7,6 @@ from viz.stylesheets import default_stylesheet
 from dash.dependencies import Input, Output
 from views import my_blueprint
 
-
-
 app = Flask(__name__)
 app.register_blueprint(my_blueprint)
 
@@ -19,25 +17,47 @@ dash_app.layout = cyto.Cytoscape(
     id='cytoscape-graph',
     style={'width': '100%', 'height': '100vh'},
     elements=get_elements(default_query),
+    layout={
+        'name': 'concentric',
+        'minNodeSpacing': 80,
+        'spacingFactor': 1.5,
+        'animate': False,
+        'animationDuration': 500,
+        'animationEasing': 'ease-in-out',
+        'boundingBox': {'x1': -500, 'y1': -500, 'x2': 500, 'y2': 500},
+    },
+
     stylesheet=default_stylesheet,
 )
 
 
+def get_concentricity(ele):
+    return ele.get('data').get('concentricity', 1)
+
 @app.route('/update')
 def update_elements():
-    location = request.args.get('location')
-    lifecycle = request.args.get('lifecycle')
-    updated_query = query_builder(location, lifecycle)
+    location = request.args.getlist('location')
+    lifecycle = request.args.getlist('lifecycle')
+    services = request.args.getlist('services')
+
+    updated_query = query_builder(location=location, lifecycle=lifecycle, services=services)
     updated_elements = get_elements(updated_query)
     dash_app.layout = cyto.Cytoscape(
         id='cytoscape-graph',
         style={'width': '100%', 'height': '100vh'},
         elements=updated_elements,
+        layout={
+                    'name': 'concentric',
+                    'minNodeSpacing': 80,
+                    'spacingFactor': 1.5,
+                    'animate': False,
+                    'animationDuration': 500,
+                    'animationEasing': 'ease-in-out',
+                    'boundingBox': {'x1': -500, 'y1': -500, 'x2': 500, 'y2': 500}
+                },
         stylesheet=default_stylesheet,
     )
     return dash_app.index()
-
-
 
 @dash_app.callback(
     Output('cytoscape-graph', 'stylesheet'),
@@ -49,7 +69,3 @@ def update_stylesheet(node_data):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
