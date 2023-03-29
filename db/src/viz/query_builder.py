43d4119dc3be5eb31ad5f21576040cea13b9a8f5
Caleb Druckemiller <cldruckemiller@gmail.com>
2023-03-28 23:19:00 -0400

    Add optional data queries
    Provide null safety to data_retriever functions

--- db/src/viz/query_builder.py
+++ db/src/viz/query_builder.py
@@ -1,8 +1,8 @@
 default_query = """
 MATCH (o:Organization)
-MATCH (o)-[:SERVICES]->(l:Location)
-MATCH (o)-[:PROVIDES]->(lc:Lifecycle)
-MATCH (o)-[:HANDLES]->(s:Service)
+OPTIONAL MATCH (o)-[:SERVICES]->(l:Location)
+OPTIONAL MATCH (o)-[:PROVIDES]->(lc:Lifecycle)
+OPTIONAL MATCH (o)-[:HANDLES]->(s:Service)
 RETURN o, l, lc, s
 """
 
@@ -18,16 +18,6 @@ def full_params_query(location="Harrisburg", lifecycle="Growth", services="Fundi
     _query += "RETURN o, l, lc, s"
     return _query
 
-# def service_expansion_query(organization_name, location, lifecycle):
-#     query = (
-#         f"MATCH (o:Organization)-[:SERVICES]->(l:Location) WHERE l.name = '{location}' "
-#         f"MATCH (o)-[:PROVIDES]->(lc:Lifecycle) WHERE lc.name = '{lifecycle}' "
-#         f"MATCH (o)-[:HANDLES]->(s:Service) WHERE o.name = '{organization_name}' "
-#         "RETURN o, l, lc, s"
-#     )
-#     print(query)
-#     return query
-
 
 def service_expansion_query(organization_name, location, lifecycle):
     query = (
@@ -38,21 +28,14 @@ def service_expansion_query(organization_name, location, lifecycle):
     )
     return query
 
-'''
-MATCH (o:Organization {name: 'Harrisburg Regional Chamber & Capital Region Economic Development Corporation'})-[:PROVIDES]->(lc:Lifecycle)
-MATCH (o)-[:SERVICES]->(l:Location)
-MATCH (o)-[:HANDLES]->(s:Service)
-RETURN o, lc, l, s
-
-'''
-
 
 def get_single_element(organization_name):
     query = (
-        f"MATCH (o:Organization {{name: '{organization_name}'}})-[:PROVIDES]->(lc:Lifecycle)\n"
-        f"MATCH (o)-[:HANDLES]->(s:Service)\n"
-        f"MATCH (o)-[:SERVICES]->(l:Location)\n"
-        f"MATCH (o)-[:HANDLES]->(s:Service)\n"
+        f"MATCH (o:Organization {{name: '{organization_name}'}})"
+        f"OPTIONAL MATCH (o)-[:PROVIDES]->(lc:Lifecycle)\n"
+        f"OPTIONAL MATCH (o)-[:HANDLES]->(s:Service)\n"
+        f"OPTIONAL MATCH (o)-[:SERVICES]->(l:Location)\n"
+        f"OPTIONAL MATCH (o)-[:HANDLES]->(s:Service)\n"
         "RETURN o, l, lc, s"
     )
     return query
